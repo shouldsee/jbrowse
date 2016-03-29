d12d7b5aae65e8cdd292169f9660778ab802b7e9
Colin <colin.diesh@gmail.com>
2016-03-29 10:14:01 -0500

    Add option for using fixBounds on scalebar

--- src/JBrowse/View/Track/Wiggle/XYPlot.js
+++ src/JBrowse/View/Track/Wiggle/XYPlot.js
@@ -48,7 +48,7 @@ var XYPlot = declare( [WiggleBase, YScaleMixin],
 
                 // update our track y-scale to reflect it
                 this.makeYScale({
-                    fixBounds: true,
+                    fixBounds: 'fixBounds' in this.config.fixBounds?this.config.fixBounds:true,
                     min: scaling.min,
                     max: scaling.max
                 });
